66577db9a12eee0683b3a581b7687b4ca67ab962
Nicholas <nickarobinson99@gmail.com>
2019-05-23 15:31:43 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var dog_chihuahua {
+	cost: 10,
+	effect: 1,
+	enabled: false,
+	active: false;
+}
+var dog_retriever {
+	cost: 100,
+	effect: 10,
+	enabled: false,
+	active: false
+}
+var dog_lab {
+	cost: 1000,
+	effect: 100,
+	enabled: false,
+	active: false
+}
+
+window.setInterval(function() {
+
+
+} 1);
